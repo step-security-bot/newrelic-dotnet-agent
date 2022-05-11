c9d239d951581f5f4a654b6d41f18af7123ec0bb
Vu Tran <vuqtran88@gmail.com>
2022-05-11 14:42:52 -0700

    initial commit

---
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/GrpcNetClient/FinishCallWrapper.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NewRelic.Agent.Api;
+using NewRelic.Agent.Api.Experimental;
+using NewRelic.Agent.Extensions.Providers.Wrapper;
+
+namespace NewRelic.Providers.Wrapper.AspNetCore.GrpcNetClient
+{
+    public class FinishCallWrapper
+    {
+        public bool IsTransactionRequired => false;
+
+        private const string WrapperName = "FinishCallWrapper";
+
+        public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
+        {
+            return new CanWrapResponse(WrapperName.Equals(methodInfo.RequestedWrapperName));
+        }
+
+        public AfterWrappedMethodDelegate BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall, IAgent agent, ITransaction transaction)
+        {
+
+            transaction.CurrentSegment.End();
+            
+            return Delegates.NoOp;
+        }
+    }
+}
---
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/GrpcNetClient/RunCallWrapper.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Net.Http;
+using NewRelic.Agent.Api;
+using NewRelic.Agent.Api.Experimental;
+using NewRelic.Agent.Extensions.Providers.Wrapper;
+
+namespace NewRelic.Providers.Wrapper.AspNetCore.GrpcNetClient
+{
+    public class RunCallWrapper
+    {
+        public bool IsTransactionRequired => false;
+
+        private const string WrapperName = "RunCallWrapper";
+
+        public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
+        {
+            return new CanWrapResponse(WrapperName.Equals(methodInfo.RequestedWrapperName));
+        }
+
+        public AfterWrappedMethodDelegate BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall, IAgent agent, ITransaction transaction)
+        {
+            if (instrumentedMethodCall.IsAsync)
+            {
+                transaction.AttachToAsync();
+            }
+
+            var httpRequestMessage = instrumentedMethodCall.MethodCall.MethodArguments[0] as HttpRequestMessage;
+
+            var method = (httpRequestMessage.Method != null ? httpRequestMessage.Method.Method : "<unknown>") ?? "<unknown>";
+
+            var transactionExperimental = transaction.GetExperimentalApi();
+
+            var externalSegmentData = transactionExperimental.CreateExternalSegmentData(httpRequestMessage.RequestUri, method);
+            var segment = transactionExperimental.StartSegment(instrumentedMethodCall.MethodCall);
+            segment.GetExperimentalApi().SetSegmentData(externalSegmentData);
+
+            return Delegates.NoOp;
+        }
+    }
+}
